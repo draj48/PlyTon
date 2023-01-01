337cbdbb875079ae06ddd81ddfab62e3b77304cb
ᗪᖇᗩᒍ <email.draj48@gmail.com>
2023-01-01 11:30:32 +0530

    dlt

--- resources/views/admin/copyright.blade.php
+++
@@ -1,1 +0,0 @@
-<footer class="footer text-right"> {{trans('words.copyright')}} © {{date('Y')}} <a href="https://team.plyton.in/" target="_blank">PlyTon Team</a>. {{trans('words.all_rights')}}. </footer>
